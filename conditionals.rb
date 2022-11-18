8441d6419d358a90dc7fdb79dd1b3b9339161f4d
Hector Garcia Rosales <ghectormus@gmail.com>
2022-11-18 08:01:26 -0600

    Adding if statement as statement modifier

--- conditionals.rb
+++ conditionals.rb
@@ -2,6 +2,10 @@ require_relative 'player'
 
 player = Player.new("JoseJuan", 150)
 
+=begin
 if player.health >= 150 
   puts "He is so powerful !!"
 end
+=end
+
+puts "He is so powerful" if player.health >= 150

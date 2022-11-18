2fc011269ae34cabcf91cf543c511ec2d83fc671
Hector Garcia Rosales <ghectormus@gmail.com>
2022-11-18 08:06:50 -0600

    Adding if else statement

--- conditionals.rb
+++ conditionals.rb
@@ -8,4 +8,10 @@ if player.health >= 150
 end
 =end
 
-puts "He is so powerful" if player.health >= 150
+#puts "He is so powerful" if player.health >= 150
+
+if player.health >= 150 
+  puts "Powerful"
+else 
+  puts "Easy"
+end

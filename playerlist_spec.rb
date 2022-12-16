639d2d953a22e1cd7516c2e6cb0d45a06e3f2136
Hector Garcia Rosales <ghectormus@gmail.com>
2022-12-16 08:53:37 -0600

    Adding tests with the :method sintax on param that stills throws a fixed number

--- playerlist_spec.rb
+++ playerlist_spec.rb
@@ -12,15 +12,18 @@ describe Playerlist do
 			@playerlist.add_player(@player)
 		end
 		it "gives the health a +1 if high number is rolled" do
-			@playerlist.start_game(5)
+			@playerlist.stub(:roll_die).and_return(5)
+			@playerlist.start_game
 			expect(@player.health).to eq(@initial_health + 1)
 		end
 		it "Does nothing if a medium number is rolled" do 
-			@playerlist.start_game(3)
+			@playerlist.stub(:roll_die).and_return(3)
+			@playerlist.start_game
 			expect(@player.health).to eq(@initial_health)
 		end
 		it "Gives the health a -1 if low number is rolled " do 
-			@playerlist.start_game(1)
+			@playerlist.stub(:roll_die).and_return(1)
+			@playerlist.start_game
 			expect(@player.health).to eq(@initial_health - 1)
 		end
 	end
